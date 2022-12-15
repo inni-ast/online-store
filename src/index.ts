b3e8eccc5b15d4330aa0e3712dfec20a297d8274
inni-ast <inni_@tut.by>
2022-12-15 15:09:18 +0300

    feat: add config

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import "./index.html";
+import "./index.scss";
+import { DATA } from "./modules/data";
+console.log(DATA[1]);
+const sum = (a: number, b: number) => {
+  return a * b;
+};
+sum(5, 6);
+console.log("hello");
