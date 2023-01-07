e97920fa8caaf8e7e3246d2bfc908048a078f07d
inni-ast <inni_@tut.by>
2023-01-07 11:35:46 +0300

    feat: add product card

--- src/pages/product.ts
+++ src/pages/product.ts
@@ -1,18 +1,15 @@
 import { Page } from "../core/templates/page";
-// import { DATA } from "../modules/data";
+import { DATA, StorageProducts } from "../modules/data";
+import { localStorageUtil } from "../modules/localStorage";
+// import { header } from "./header";
 
-const H3 = [
-  "Description",
-  "Discount Percentage:",
-  "Rating:",
-  "Stock:",
-  "Brand:",
-  "Category:",
-];
 export class Product extends Page {
   static TextObject = {
     mainTitle: "Product",
     total: "Product total",
+    addToCard: "Add to card",
+    buyNow: "BUY NOW",
+    dropFromCard: "Drop from card",
   };
 
   productContainer: HTMLElement;
@@ -25,9 +22,6 @@ export class Product extends Page {
   addToCart: HTMLElement;
   productSlides: HTMLElement;
   productBigPhoto: HTMLElement;
-  // productDetailItem: HTMLElement;
-  // productDetailH3: HTMLElement;
-  // productDetailP: HTMLElement;
 
   constructor(el: string, id: string, nameClass: string) {
     super(el, id, nameClass);
@@ -36,7 +30,6 @@ export class Product extends Page {
 
     this.productLine = document.createElement("div");
     this.productLine.classList.add("product__line");
-
     this.productDetail = document.createElement("div");
     this.productDetail.classList.add("product__detail");
 
@@ -72,25 +65,145 @@ export class Product extends Page {
 
     this.productPhotos.append(this.productSlides, this.productBigPhoto);
   }
-  getProduct(id: number) {
-    for (let i = 0; i < 6; i++) {
+  getProduct(idi: number) {
+    const id = idi - 1;
+    const H3 = [
+      "Description",
+      "Discount Percentage:",
+      "Rating:",
+      "Stock:",
+      "Brand:",
+      "Category:",
+    ];
+    const P = [
+      DATA[id].description,
+      DATA[id].discountPercentage,
+      DATA[id].rating,
+      DATA[id].stock,
+      DATA[id].brand,
+      DATA[id].category,
+    ];
+
+    const linkStore1 = document.createElement("a");
+    linkStore1.setAttribute("href", "#main-container");
+    linkStore1.textContent = "STORE";
+    this.productLine.append(linkStore1);
+    const textStore1 = document.createElement("p");
+    textStore1.textContent = " >> ";
+    this.productLine.append(textStore1);
+    const linkStore2 = document.createElement("a");
+    linkStore2.setAttribute("href", "#");
+    linkStore2.textContent = `${DATA[id].category.toUpperCase()}`;
+    this.productLine.append(linkStore2);
+    const textStore2 = document.createElement("p");
+    textStore2.textContent = " >> ";
+    this.productLine.append(textStore2);
+    const linkStore3 = document.createElement("a");
+    linkStore3.setAttribute("href", "#");
+    linkStore3.textContent = `${DATA[id].brand.toUpperCase()}`;
+    this.productLine.append(linkStore3);
+    const textStore3 = document.createElement("p");
+    textStore3.textContent = " >> ";
+    this.productLine.append(textStore3);
+    const linkStore4 = document.createElement("a");
+    linkStore4.setAttribute("href", "#");
+    linkStore4.textContent = `${DATA[id].title.toUpperCase()}`;
+    this.productLine.append(linkStore4);
+
+    this.productTitle.textContent = `${DATA[id].title.toUpperCase()}`;
+
+    for (let i = 0; i < DATA[id].images.length; i++) {
+      const productSlidesPhoto = document.createElement("img");
+      productSlidesPhoto.setAttribute("src", `${DATA[id].images[i]}`);
+      this.productSlides.append(productSlidesPhoto);
+    }
+
+    const productBigPhoto = document.createElement("img");
+    productBigPhoto.setAttribute("src", `${DATA[id].thumbnail}`);
+    this.productBigPhoto.append(productBigPhoto);
+
+    for (let i = 0; i < H3.length; i++) {
       const productDetailItem = document.createElement("div");
       productDetailItem.classList.add("product__detail-item");
 
       const productDetailH3 = document.createElement("h3");
-      productDetailH3.textContent = H3[id - 1];
+      productDetailH3.textContent = `${H3[i]}`;
 
       const productDetailP = document.createElement("p");
-      // productDetailH3.textContent=DATA[id].H3[id-1];
+      productDetailP.textContent = `${P[i]}`;
 
       productDetailItem.append(productDetailH3, productDetailP);
       this.productInfo.append(productDetailItem);
     }
+
+    const productPrice = document.createElement("p");
+    productPrice.classList.add("product__price");
+    productPrice.textContent = "€" + DATA[id].price;
+
+    const productBasket = document.createElement("button");
+
+    productBasket.classList.add(`card__btn`);
+
+    const productsStore = localStorageUtil
+      .getProducts()
+      .map((x: StorageProducts) => x.id);
+
+    let activeClass = "";
+    let activeText = "";
+
+    if (productsStore.indexOf(id + 1) === -1) {
+      activeText = Product.TextObject.addToCard;
+    } else {
+      activeText = Product.TextObject.dropFromCard;
+      activeClass = "active-btn";
+      productBasket.classList.add(`${activeClass}`);
+    }
+
+    productBasket.setAttribute("data-id", `${DATA[id].id}`);
+    productBasket.setAttribute("data-price", `${DATA[id].price}`);
+    productBasket.textContent = activeText;
+
+    const productBuy = document.createElement("button");
+    productBuy.classList.add(`card__btnB`);
+    productBuy.textContent = Product.TextObject.buyNow;
+
+    this.addToCart.append(productPrice, productBasket, productBuy);
   }
   render() {
     // const title = this.createTitle(Product.TextObject.mainTitle);
     this.container.append(this.productContainer);
+    const id = 4;
+    this.getProduct(id);
     return this.container;
   }
 }
+
 export const PRODUCT = new Product("div", "products", "products");
+
+// window.onload = function () {
+//   (document.getElementById("products") as HTMLElement).onclick = function (
+//     event: Event
+//   ) {
+//     console.log("1");
+//     const target = event.target as HTMLElement;
+//     const id = target.getAttribute("data-id");
+//     const price = target.getAttribute("data-price");
+
+//     if (id && price) {
+//       if (target.classList.contains("card__btn")) {
+//         const { pushProduct } = localStorageUtil.putProducts(+id, +price);
+
+//         if (pushProduct) {
+//           target.classList.add("active-btn");
+//           target.innerHTML = Product.TextObject.dropFromCard;
+
+//           header.addProduct(+price);
+//         } else {
+//           target.classList.remove("active-btn");
+//           target.innerHTML = Product.TextObject.addToCard;
+//           header.removeProduct(+price);
+//         }
+//       }
+//     }
+//   };
+// };
