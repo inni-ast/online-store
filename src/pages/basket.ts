37377c16aa1bc20e8232782ec0764f77e62e46ad
inni-ast <inni_@tut.by>
2023-01-07 10:46:51 +0300

    feat: add validation

--- src/pages/basket.ts
+++ src/pages/basket.ts
@@ -3,6 +3,7 @@ import { DATA } from "../modules/data";
 import { localStorageUtil } from "../modules/localStorage";
 import { StorageProducts } from "../modules/data";
 import { header } from "./header";
+// import { Visa } from "../img/visa.png";
 
 const overlay = document.getElementById("overlay-modal") as HTMLDivElement;
 export class Basket extends Page {
@@ -97,31 +98,45 @@ export class Basket extends Page {
     const html = `
       <div class="form-buy_close"> X </div>
       <h4 class="form-buy__title"> Personal details</h4>
+
       <input type="text" id="buy-name" name="name" required
-          placeholder="Name, Surname" class="form-buy__input">
-      <input type="number" id="buy-tel" name="tel" required
-          placeholder="Phone number" class="form-buy__input">
+          placeholder="Name, Surname" class="form-buy__input"
+          pattern="([a-zA-Zа-яА-Я]{3,})[ ]([a-zA-Zа-яА-Я]{3,})(([a-zA-Zа-яА-Я ]{0,}){0,})">
+
+      <input type="tel" id="buy-tel" name="tel" required
+          placeholder="Phone number" class="form-buy__input"
+          pattern="[+]([0-9]{9,})">
+
       <input type="text" id="buy-address" name="address" required
-          placeholder="Your address" class="form-buy__input">
+          placeholder="Your address" class="form-buy__input"
+          pattern="([a-zA-Zа-яА-Я]{5,})[ ]([a-zA-Zа-яА-Я]{5,})[ ]([a-zA-Zа-яА-Я]{5,})(([a-zA-Zа-яА-Я ]{0,}){0,})">
+
       <input type="email" id="buy-email" name="email" required
           placeholder="Your email" class="form-buy__input">
+
       <div class="form-buy__card form-card">
           <h4 class="form-card__title"> Credit card details</h4>
             <div class="form-card__block">
               <div class="form-card__number">
                   <div class="form-card__image">
-                    <img src="#" class="form-card__img">
+                    <img src="https://i.guim.co.uk/img/media/b73cc57cb1d46ae742efd06b6c58805e8600d482/16_0_2443_1466/master/2443.jpg?width=700&quality=85&auto=format&fit=max&s=fb1dca6cdd4589cd9ef2fc941935de71" class="form-card__img">
                   </div>
-              <input type="number" id="card-num" name="card-num" required
-                  placeholder="Card number" class="form-card__input-number">
+
+              <input type="text" id="card-num" name="card-num" required
+                  placeholder="Card number" class="form-card__input-number"
+                  pattern="([0-9]{16})">
               </div>
+
             <label class="form-card__label">
               VALID:
-               <input type="number" id="card-valid" name="card-valid" required placeholder="Data" class="form-card__input-data">
-            </label>
+               <input type="text" id="card-valid" name="card-valid" required placeholder="Data" class="form-card__input-data"
+               pattern="(0[1-9]|1[1-2])/[0-9]{2}">
+               </label>
             <label class="form-card__label">
               CVV:
-               <input type="number" id="card-cvv" name="card-cvv" required placeholder="CVV" class="form-card__input-cvv">
+               <input type="text" id="card-cvv" name="card-cvv" required
+               placeholder="CVV" class="form-card__input-cvv"
+               pattern="([0-9]{3})">
             </label>
            </div>
       </div>
